cec228411d61fe966e73f3c8d71157bebfa8feac
oliv-bernier <olivbernier@gmail.com>
2022-02-20 23:28:20 +0100

    add prettier

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,22 @@
 
 const app = {
   rootElement: document.querySelector<HTMLDivElement>('#app'),
-  init: function() {
+  init: function () {
     const { hello, title } = app.getTitle()
-    
+
     app.rootElement.prepend(title)
     console.log(hello)
   },
-  getTitle: function() {
+  getTitle: function () {
     const title: HTMLHeadingElement = document.createElement('h1')
     title.classList.add('project__title')
     title.textContent = 'Simple Frontend Project'
 
     const img: HTMLImageElement = document.createElement('img')
+
     img.classList.add('vite__logo')
     img.src = '../assets/vitejs.svg'
+
     img.setAttribute('alt', 'Vite JS logo')
 
     title.appendChild(img)
@@ -25,6 +27,5 @@ const app = {
     return { title, hello }
   },
 }
-
 
 document.addEventListener('DOMContentLoaded', app.init)
