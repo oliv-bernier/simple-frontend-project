7ccc848f15ed52c060d36045274240556919cdea
oliv-bernier <olivbernier@gmail.com>
2022-02-20 12:49:10 +0100

    first version

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+// Write here all your code in TypeScript or JavaScript but still benefit from TypeScript's IntelliSense!
+
+const app = {
+  rootElement: document.querySelector<HTMLDivElement>('#app'),
+  init: function() {
+    const { hello, title } = app.getTitle()
+    
+    app.rootElement.prepend(title)
+    console.log(hello)
+  },
+  getTitle: function() {
+    const title: HTMLHeadingElement = document.createElement('h1')
+    title.classList.add('project__title')
+    title.textContent = 'Simple Frontend Project'
+
+    const img: HTMLImageElement = document.createElement('img')
+    img.classList.add('vite__logo')
+    img.src = '../assets/vitejs.svg'
+    img.setAttribute('alt', 'Vite JS logo')
+
+    title.appendChild(img)
+
+    const hello: string = 'Hello world!'
+
+    return { title, hello }
+  },
+}
+
+
+document.addEventListener('DOMContentLoaded', app.init)
